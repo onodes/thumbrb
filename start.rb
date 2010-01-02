e5d680659566a2793149e003b1c0642e1ef1e058
onodes <onodes@Balthazar.fletsphone>
2010-01-03 04:21:57 +0900

    for quad core three thred

--- start.rb
+++ start.rb
@@ -2,14 +2,60 @@ require 'rubygems'
 gem PLATFORM =='java'?'rmagick4j' : 'rmagick'
 require'RMagick'
 require'thumb'
+require 'pp'
+require 'thread'
+
+q = Queue.new
 
 dir_r = ARGV.to_s.chomp
 dir_w = dir_r.gsub("albums","cache")
 
 
-ary = Dir::glob(dir_r + "*.{jpg,JPG}")
-p ary2 = ary.map{|filename| filename.gsub("albums","cache")}
-data = Thumb.new(ary[0],ary2[0])
-data.convert
+ary = Dir::glob(dir_r + "*.{jpg,JPG}").map{|filename| Thumb.new(filename)}
 
+puts
+puts
+puts "-"*30
+puts "-"*3 + "For zenphoto On PHP Web Album" + "-"*3
+puts "Created by onodes 2009-2010"
+puts 
+puts "-"*30
 
+sleep 1.5
+
+ary.each{|r|
+  q.push(r)
+}
+
+t1 = Thread.new{
+  while(!q.empty?)
+    print "Thread1 : "
+    instance = q.pop
+    instance.convert
+  end
+  self.kill
+}
+
+t2 = Thread.new{
+  while(!q.empty?)
+    print "Thread2 : "
+    instance = q.pop
+    instance.convert
+  end
+  self.kill
+}
+
+t3 = Thread.new{
+  while(!q.empty?)
+    print "Thread3 : "
+    instance = q.pop
+    instance.convert
+  end
+  self.kill
+}
+
+t1.join
+t2.join
+t3.join
+
+puts "End"
--- thumb.rb
+++ thumb.rb
@@ -4,9 +4,9 @@ require 'RMagick'
 
 class Thumb
   
-  def initialize(path_r,path_w,opt='')
+  def initialize(path_r,opt='')
     @path_r = path_r
-    @path_w = path_w
+    @path_w = path_r.gsub("albums","cache")
   end
 
   def split_ext(file)
@@ -18,7 +18,6 @@ class Thumb
  
   def rename(filename,add)
     name=  split_ext(filename)[0] + '/' + split_ext(filename)[1] + add + split_ext(filename)[2]
-    puts name
     return name
   end
 
@@ -31,6 +30,6 @@ class Thumb
     new_img2 = img.resize(595,398)
     new_img2.write(rename(@path_w,'_595'))
 
-    return @path_r
+    puts split_ext(@path_r)[1]+"created"
   end
 end 
